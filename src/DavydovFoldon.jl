550c9ac56a85e5f73b7a4c1bffaa46b0b36998e5
NotAParlayMaker <michaelmannen3@gmail.com>
2026-08-01 14:41:30 -0400

    Export feasibility planning API

--- src/DavydovFoldon.jl
+++ src/DavydovFoldon.jl
@@ -12,7 +12,9 @@ include("FoldonDB.jl")
 export DavydovParams, Trajectory, aa_to_params, run_soliton, save_trajectory,
        contact_map, animate_soliton, SpectrumParams, Spectra, build_hamiltonian,
        compute_2DIR_spectrum, compute_2DIR, extract_beat_frequency,
-       predict_signal_amplitude, design_pulse_sequence, analyze_beats,
-       codon_optimizer, open_database, store_simulation!, serve
+       FeasibilityParams, estimate_transient_population, assess_2dir_feasibility,
+       staged_validation_plan, predict_signal_amplitude, design_pulse_sequence,
+       design_reaction_scan, analyze_beats, codon_optimizer, open_database,
+       store_simulation!, serve
 
 end
